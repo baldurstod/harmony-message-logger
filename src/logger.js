6d866a7a25365f06d6edc7cc7237e2ccf496af81
Guillaume <Guillaume>
2023-04-11 16:15:53 +0200

    Added logger retention

--- src/logger.js
+++ src/logger.js
@@ -2,38 +2,49 @@ import Datastore from '@seald-io/nedb';
 import {setTimeoutPromise} from 'harmony-utils';
 
 const AUTO_PURGE_DELAY = 15 * 60 * 1000;
+const DEFAULT_RETENTION = 30 * 24 * 60 * 60;
 
 export class Logger {
 	#loggerDB;
 	#closePromise;
 	#closePromiseResolve;
-	constructor({databasePath, autoPurge} = {}) {
+	#retention;
+	#abortController;
+	constructor({databasePath, retention = DEFAULT_RETENTION} = {}) {
+		this.#retention = retention;
 		this.#closePromise = new Promise(resolve => this.#closePromiseResolve = resolve);
 		if (!databasePath) {
 			throw 'No database path provided';
 		}
 		this.#loggerDB = new Datastore({filename: databasePath, autoload: true});
-		if (autoPurge) {
-			this.#autoPurge();
-		}
+		this.#autoPurge();
 	}
 
 	close() {
 		this.#closePromiseResolve('closed');
+		this.#abortController?.abort();
 	}
 
 	async #autoPurge() {
+		this.#abortController = new AbortController();
+		const signal = this.#abortController.signal;
 		await this.purgeOldMessages();
-		const raceResult = await Promise.race([this.#closePromise, setTimeoutPromise(AUTO_PURGE_DELAY)]);
-		if (raceResult != 'closed') {
-			this.#autoPurge();
-		}
+		try {
+			const raceResult = await Promise.race([this.#closePromise, setTimeoutPromise(AUTO_PURGE_DELAY, signal)]);
+			if (raceResult != 'closed') {
+				this.#autoPurge();
+			}
+		} catch (e) {}
 	}
 
 	async purgeOldMessages() {
 		try {
 			const now = Date.now();
 			await this.#loggerDB.removeAsync({deleteAfter: {$lt: now}}, {multi: true});
+			if (this.#retention !== 0) {
+				const createdBefore = now - this.#retention * 1000;
+				await this.#loggerDB.removeAsync({dateCreated: {$lt: createdBefore}}, {multi: true});
+			}
 
 			await this.#loggerDB.compactDatafileAsync();
 		} catch (e) {}
